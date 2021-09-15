d4fcff3d78803055fb11f29f826936406c225593
Weret90 <apostol-ea@mail.ru>
2021-09-15 12:46:50 +0900

    Добавил стартовую тему с логотипом на черном фоне, добавил вторую красную тему, добавил шрифт limbo.ttf к второй теме, исправил ошибку при которой приложение не получало респонс ночью из-за разницы часовых поясов

--- app/src/main/java/com/umbrella/nasaapiapp/view/MainActivity.kt
+++ app/src/main/java/com/umbrella/nasaapiapp/view/MainActivity.kt
@@ -7,6 +7,7 @@ import com.umbrella.nasaapiapp.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_NASAApiApp)
         setContentView(R.layout.activity_main)
     }
 }
